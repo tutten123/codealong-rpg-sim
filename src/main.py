86ed8fb91dbae92f1dfb1db9b812de8018aa8913
tutten123 <oliwertoth@gmail.com>
2022-02-11 13:35:02 +0100

    Added goblin and fight function

--- src/main.py
+++ src/main.py
@@ -1,12 +1,33 @@
-from resources import Character
+from resources import Character, Goblin
+import random
+def fight(fighter:Character, enemies: list):
+
+    while not len(enemies) == 0:
+        fighter_target = random.choice(enemies)
+        fighter_target.take_damage(fighter.attack())
+        if fighter_target.get_health() == 0:
+            print("Target has died!")
+            enemies.remove(fighter_target)
+            if len(enemies) == 0: break
+    print(f"fight is over! {fighter.name} won!")
 
 def main():
+    enemies = []
+
     nick = Character("Nick", 15, 3, 1)
     emy = Character("Emy", 20, 6, 5)
 
     print(nick)
     print()
     print(emy)
+
+
+    enemies.append(Goblin(1))
+    print("\nGoblin")
+    print(enemies[0])
+
+    fight(emy, enemies)
+
 
 if __name__ == "__main__":
     main()
--- src/resources.py
+++ src/resources.py
@@ -11,7 +11,40 @@ class Character:
     
     def __str__(self):
         return f"Name: {self.name}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}"
+    
+    def take_damage(self, dmg):
+        actual_damage = dmg - self.armor
         
+        if self.health - actual_damage < 0: self.health = 0
+        
+        else: self.health -= actual_damage
+
+    def attack(self):
+        return self.damage
+    
+    def get_health(self):
+        return self.health
+class Goblin:
+
+    def __init__(self, id):
+        self.id = id
+        self.health = 10
+        self.damage = 3
+        self.armor = 2
+    
+    def __str__(self):
+        return f"ID: {self.id}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}"
+    def take_damage(self, dmg):
+        actual_damage = dmg - self.armor
+        
+        if self.health - actual_damage < 0: self.health = 0
+        
+        else: self.health -= actual_damage
+    def attack(self):
+        return self.damage
+    
+    def get_health(self):
+        return self.health
 # functions
 
 # main code
